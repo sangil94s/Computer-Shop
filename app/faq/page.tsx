40f0ea9846585bc8d2460cb2a5012bcec218bed7
sangil94s <bsc7417@gmail.com>
2025-02-24 20:20:37 +0900

    [Add] 폴더 초안 추가

---
+++ app/faq/page.tsx
@@ -0,0 +1,8 @@
+// 아마도 FAQ를 보여주는 부분
+export default function page() {
+  return (
+    <>
+      <h1>FAQ</h1>
+    </>
+  );
+}
---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+// 로딩시 보여지는 부분
+
+export default function loading() {
+  return (
+    <>
+      <p className="text-center text-blue-500 font-bold py-4">로딩중...</p>
+    </>
+  );
+}
