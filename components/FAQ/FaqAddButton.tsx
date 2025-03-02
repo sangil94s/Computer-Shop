15fd0b6c5a9794c705a2fce5af1161a7e076b4a2
sangil94s <bsc7417@gmail.com>
2025-03-02 18:50:25 +0900

    [Modify] 로그인한 사용자가 관리자 권한값과 일치할 경우만 버튼 활성화로 수정

--- components/FAQ/FaqAddButton.tsx
+++ components/FAQ/FaqAddButton.tsx
@@ -2,14 +2,17 @@
 'use client';
 import { Button } from '@/components/ui/button';
 import { useRouter } from 'next/navigation';
-
+import { useSession } from 'next-auth/react';
 export default function FaqAddButton() {
   const router = useRouter();
+  const { data: session } = useSession();
   return (
     <>
-      <Button className="m-1" onClick={() => router.push('/faq/add')}>
-        관리자 전용 - 추가
-      </Button>
+      {session?.user.name === process.env.NEXT_PUBLIC_ADMIN_AUDIT && (
+        <Button className="m-1" onClick={() => router.push('/faq/add')}>
+          관리자 전용 - 추가
+        </Button>
+      )}
     </>
   );
 }
