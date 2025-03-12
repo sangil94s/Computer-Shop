ad31ecc882632670868aea9f180eba97bd90a65a
sangil94s <bsc7417@gmail.com>
2025-03-12 17:17:18 +0900

    [Modify] 상품 상세페이지 동적 Metadata 추가

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -3,12 +3,23 @@ import ProductDetailAddButton from '@/components/Product/Detil/ProductDetailAddB
 import ProductDetailCard from '@/components/Product/ProductDetailCard';
 import ProductDetailImage from '@/components/Product/Detil/ProductDetailImage';
 import ProductDetailMainImage from '@/components/Product/Detil/ProductDetailMainImage';
-
 import { Metadata } from 'next';
-export const metadata: Metadata = {
-  title: '상품 상세 페이지 | Computer-Shop',
-  description: '수정 예정',
-};
+
+type Params = Promise<{ id: string }>;
+export async function generateMetadata({ params }: { params: Params }): Promise<Metadata> {
+  const {id} = await params;
+  const res = await fetch(`${process.env.NEXT_PUBLIC_DEPLOY_URL}/api/product/${id}`, { cache: 'no-store' });
+
+  if (!res.ok) {
+    throw new Error('데이터 호출 실패');
+  }
+  const data = await res.json();
+
+  return {
+    title: `${data.title} | Computer-Shop`,
+    description: data.description || '상품 상세 페이지입니다.',
+  };
+}
 
 export default async function page({ params }: { params: Promise<{ id: string }> }) {
   const id = (await params).id;
