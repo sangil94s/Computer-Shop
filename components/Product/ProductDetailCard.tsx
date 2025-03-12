2eb71225cfe429ecdf5dd57f471db581ab86a544
sangil94s <bsc7417@gmail.com>
2025-03-12 21:07:03 +0900

    [Add] Select에 따라 최종 가격 변하도록 수정

--- components/Product/ProductDetailCard.tsx
+++ components/Product/ProductDetailCard.tsx
@@ -12,7 +12,7 @@ import {
 } from '@/components/ui/select';
 import { useQuery } from '@tanstack/react-query';
 import { useSession } from 'next-auth/react';
-
+import { useState, useEffect } from 'react';
 // 상품 상세 페이지에 상품 정보가 들어갈 부분
 
 const fetchProducts = async ({ id }: { id: string }) => {
@@ -29,6 +29,15 @@ export default function ProductDetailCard({ id }: { id: string }) {
 
   const { data: session } = useSession();
 
+  const [quantity, setQuantity] = useState<number>(1);
+  const [totalPrice, setTotalPrice] = useState<number>(products?.price || 0);
+
+  useEffect(() => {
+    if (products) {
+      setTotalPrice(products.price * quantity);
+    }
+  }, [quantity, products]);
+
   return (
     <>
       <section className="w-full h-max">
@@ -39,7 +48,7 @@ export default function ProductDetailCard({ id }: { id: string }) {
 
           <div className="my-1 w-6/12">
             <label className="font-bold text-base">수량을 선택하시오</label>
-            <Select>
+            <Select onValueChange={value => setQuantity(Number(value))}>
               <SelectTrigger>
                 <SelectValue placeholder="수량을 선택하세요" />
               </SelectTrigger>
@@ -56,7 +65,7 @@ export default function ProductDetailCard({ id }: { id: string }) {
             </Select>
           </div>
 
-          <h3 className="py-4 text-2xl font-bold">최종 가격 : 원</h3>
+          <h3 className="py-4 text-2xl font-bold">최종 가격 : {totalPrice.toLocaleString()} 원</h3>
 
           {products?.purchase === true && (
             <Link className="w-max" href="/cart">
