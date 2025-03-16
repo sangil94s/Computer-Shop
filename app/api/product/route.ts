3e1686d45f1ddaf4050b41abc5a42cc944e3e245
sangil94s <bsc7417@gmail.com>
2025-03-16 14:21:02 +0900

    [Add] 장바구니 Model 초안 추가

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -1,6 +1,15 @@
 import prisma from '@/lib/prisma';
 import { NextRequest, NextResponse } from 'next/server';
+import { z } from 'zod';
 
+const ProductSchema = z.object({
+  title: z.string().min(5, '상품명은 최소 5글자 이상이어야 한다'),
+  category: z.string().min(3, '카테고리는 3자 이상이어야한다'),
+  smallDescription: z.string().min(5, '상품 간단 설명은 5자 이상이어야 한다'),
+  productImage: z.string().min(7, '이미지'),
+  price: z.number().int().min(0, '가격은 0 이상이어야 한다.'),
+  purchase: z.boolean(),
+});
 export async function GET() {
   try {
     const data = await prisma.product.findMany();
@@ -14,27 +23,19 @@ export async function GET() {
 export async function POST(request: NextRequest) {
   try {
     const body = await request.json();
-    if (
-      !body ||
-      !body.category ||
-      !body.title ||
-      !body.price ||
-      body.purchase === undefined ||
-      !body.smallDescription ||
-      !body.productImage
-    ) {
-      return NextResponse.json({ message: '필수 값이 누락되었습니다.' }, { status: 400 });
+    const parsed = ProductSchema.safeParse(body);
+    if (!parsed.success) {
+      return NextResponse.json(
+        {
+          message: '미 유효 데이터',
+          errors: parsed.error.format(),
+        },
+        { status: 400 },
+      );
     }
 
     const newProduct = await prisma.product.create({
-      data: {
-        category: body.category,
-        title: body.title,
-        price: body.price,
-        purchase: body.purchase,
-        smallDescription: body.smallDescription,
-        productImage: body.productImage,
-      },
+      data: parsed.data,
     });
     return NextResponse.json(newProduct);
   } catch (e) {
