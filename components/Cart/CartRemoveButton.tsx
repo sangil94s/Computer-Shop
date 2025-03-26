56eb4b9ba64018a544cd92644eed3592e7042268
sangil94s <bsc7417@gmail.com>
2025-03-26 17:30:19 +0900

    [Add] 장바구니 삭제 추가

--- components/Cart/CartRemoveButton.tsx
+++ components/Cart/CartRemoveButton.tsx
@@ -3,12 +3,27 @@
 
 import { Button } from '../ui/button';
 import { AiOutlineClose } from 'react-icons/ai';
+import axios from 'axios';
+import { useSession } from 'next-auth/react';
 
-export default function CartRemoveButton() {
+export default function CartRemoveButton({ id }: { id: number }) {
+  const { data: session } = useSession();
+  const handleDelete = async () => {
+    if (!confirm('이 장바구니 상품을 삭제할 건가요???')) {
+      return;
+    }
+    try {
+      await axios.delete(`${process.env.NEXT_PUBLIC_DEPLOY_URL}/api/cart/${session?.user.name}/${id}`);
+      alert('삭제 성공!');
+      location.reload();
+    } catch (e) {
+      console.error(e);
+    }
+  };
   return (
     <>
-      <Button className="my-2">
-        <AiOutlineClose /> 전체 삭제
+      <Button className="my-2" onClick={() => handleDelete()}>
+        <AiOutlineClose /> 삭제
       </Button>
     </>
   );
